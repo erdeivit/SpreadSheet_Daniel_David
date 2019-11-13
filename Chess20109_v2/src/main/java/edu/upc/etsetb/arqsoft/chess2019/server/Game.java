a287843ba42a656d8c2f3bf44851b523a86ad163
Daniel <dalego1997@gmail.com>
2019-11-13 17:45:23 +0100

    Chess: REFACTORED CODE V2, PRELIMINARY PHASE

---
+++ Chess20109_v2/src/main/java/edu/upc/etsetb/arqsoft/chess2019/server/Game.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.upc.etsetb.arqsoft.chess2019.server;
+
+/**
+ *
+ * @author JuanCarlos
+ */
+public class Game {
+
+    private ServerProtocolMngr protMngr;
+    Board board;
+    Player player_white;
+    Player player_black;
+
+    public Game() {
+        
+        this.board = new Board();
+        this.player_black = new Player(Color.BLACK,board);
+        this.player_white = new Player(Color.WHITE,board);
+        
+    }
+    
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public Player getPlayer_white() {
+        return player_white;
+    }
+
+    public void setPlayer_white(Player player_white) {
+        this.player_white = player_white;
+    }
+
+    public Player getPlayer_black() {
+        return player_black;
+    }
+
+    public void setPlayer_black(Player player_black) {
+        this.player_black = player_black;
+    }
+    
+
+    public void setServerProtMngr(ServerProtocolMngr protMngr) {
+        this.protMngr = protMngr;
+    }
+
+    public void move(int rO, int cO, int rD, int cD) {
+        /*
+        Initial version: it just sends back an OK message to the client.
+        You should modify its code for implementing the sequence diagram in the 
+        script of the lab session
+
+        NOTE: USE THE FOLLOWING UNCOMMENTED INSTRUCTION FOR SENDING AN ERROR MESSAGE TO THE CLIENT.
+        AN ERROR MESSAGE SHALL BE AN END-OF-LINE FREE STRING STARTING WITH "E "
+         */
+        //         this.protMngr.sendFromServerToClient("E this is an error message");
+
+        
+        
+        /* 
+        DO NOT CHANGE THE CODE BELOW.
+        FINAL PART OF THE METHOD. IF ARRIVED HERE, THE MOVEMENT CAN BE PERFORMED
+        METHOD assessCheckOrCheckMate(...) CHECKS WHETHER THERE IS CHECK OR CHECK-MATE.
+        IF IT IS CHECK-MATE THE METHOD RETURNS TRUE AND A NON-EMPTY STRING
+        IF THERE IS CHECK THE METHOD RETURNS FALSE AND A NON-EMPTY STRING
+        IF THERE IS NONE OF BOTH, THE METHOD RETURNS FALSE AND AN EMPTY STRING
+         */
+        StringBuilder assessMess = new StringBuilder();
+        boolean isCheckMate = this.assessCheckOrCheckMate(assessMess);
+
+        if (assessMess.length() == 0) {
+            this.protMngr.sendFromServerToClient("OK");
+        } else {
+            this.protMngr.sendFromServerToClient(assessMess.insert(0, "OK\n").toString());
+        }
+        if(isCheckMate){
+            this.proceedToFinalizedGame() ;
+        }
+
+    }
+
+    private boolean assessCheckOrCheckMate(StringBuilder assessMess) {
+        // IF THE PROGRAM SHOULD BE COMPLETED, IT SHOULD BE IMPLEMENTED
+        return false ;
+    }
+
+    private void proceedToFinalizedGame() {
+        // IF THE PROGRAM SHOULD BE COMPLETED, IT SHOULD BE IMPLEMENTED
+        return ;
+    }
+    
+    public static void main(String args[]){
+        Game game = new Game();
+        System.out.println("GAME CREATED");
+    }
+ 
+}
